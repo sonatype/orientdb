675f008b4bbaac962f15171146a7d360c09d1a69
Stuart McCulloch <mcculls@gmail.com>
2017-01-04 00:52:41 +0000

    Avoid misleading error message when file lock is disabled

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/OPaginatedStorageDirtyFlag.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/OPaginatedStorageDirtyFlag.java
@@ -159,7 +159,7 @@ public class OPaginatedStorageDirtyFlag {
         if (fileLock != null) {
           fileLock.release();
           fileLock = null;
-        } else {
+        } else if (OGlobalConfiguration.FILE_LOCK.getValueAsBoolean()) {
           OLogManager.instance().error(this, "File lock is null , file lock is not released");
         }
 
